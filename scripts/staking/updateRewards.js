e8d2b4a46012b9a2149b2075816df5ba1ce7cd76
dancube <denright@gmail.com>
2025-01-23 12:48:14 +1100

    rewards distribution scripts updated

--- scripts/staking/updateRewards.js
+++ scripts/staking/updateRewards.js
@@ -1,8 +1,8 @@
 const { getFrameSigner, contractAt, sendTxn, updateTokensPerInterval } = require("../shared/helpers")
 
-const network = (process.env.HARDHAT_NETWORK || 'mainnet');
+const network = process.argv[2] || process.env.HARDHAT_NETWORK || 'mainnet';
 
-async function getArbValues(signer) {
+async function getPegasusValues(signer) {
   const rewardToken = await contractAt("Token", "0xF42991f02C07AB66cFEa282E7E482382aEB85461", signer)
   const tokenDecimals = 18
 
@@ -22,7 +22,7 @@ async function getArbValues(signer) {
   return { rewardToken, tokenDecimals, rewardTrackerArr }
 }
 
-async function getAvaxValues(signer) {
+async function getPhoenixValues(signer) {
   const rewardToken = await contractAt("Token", "0x7EbeF2A4b1B09381Ec5B9dF8C5c6f2dBECA59c73", signer)
   const tokenDecimals = 18
 
@@ -30,12 +30,52 @@ async function getAvaxValues(signer) {
     {
       name: "feeGmxTracker",
       address: "0xB87bc8A763A7856C5806bB0920C00c97Ca5c00d3",
-      transferAmount: "7414"
+      transferAmount: "0"
     },
     {
       name: "feeGlpTracker",
       address: "0xEE5BD4c29f4cAC23554cc3965D7aE0E22FE84BD7",
-      transferAmount: "38822"
+      transferAmount: "0.134"
+    }
+  ]
+
+  return { rewardToken, tokenDecimals, rewardTrackerArr }
+}
+
+async function getBscValues(signer) {
+  const rewardToken = await contractAt("Token", "0x2F29Bc0FFAF9bff337b31CBe6CB5Fb3bf12e5840", signer)
+  const tokenDecimals = 18
+
+  const rewardTrackerArr = [
+    {
+      name: "feeGmxTracker",
+      address: "0x4d268Bf616c768F87333c15F377B8691F8e69007",
+      transferAmount: "0"
+    },
+    {
+      name: "feeGlpTracker",
+      address: "0x5C04fa0B08c2BB5A69c91C6EF6E7ADD95E134c11",
+      transferAmount: "0"
+    }
+  ]
+
+  return { rewardToken, tokenDecimals, rewardTrackerArr }
+}
+
+async function getSonicValues(signer) {
+  const rewardToken = await contractAt("Token", "0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38", signer)
+  const tokenDecimals = 18
+
+  const rewardTrackerArr = [
+    {
+      name: "feeGmxTracker",
+      address: "0x9A4878DfAAFf4df8C640eCDCF661Bd6aF5aAFb08",
+      transferAmount: "0"
+    },
+    {
+      name: "feeGlpTracker",
+      address: "0x21efb5680d6127d6c39ae0d62d80cb9fc8935887",
+      transferAmount: "170"
     }
   ]
 
@@ -44,12 +84,22 @@ async function getAvaxValues(signer) {
 
 function getValues(signer) {
   if (network === "pegasus") {
-    return getArbValues(signer)
+    return getPegasusValues(signer)
   }
 
-  //if (network === "phoenix") {
-  //  return getAvaxValues(signer)
-  //}
+  if (network === "phoenix") {
+    return getPhoenixValues(signer)
+  }
+
+  if (network === "bsc") {
+    return getBscValues(signer)
+  }
+
+  if (network === "sonic") {
+    return getSonicValues(signer)
+  }
+
+  throw new Error("Unsupported network")
 }
 
 async function main() {
